d8cd8570e0ad1ebe2727aa9b0479b29415d562d4
micheal <mojemoron2009@yahoo.com>
2019-07-26 07:54:23 +0100

    first commit

---
+++ app/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Collect static files
+echo "Collect static files"
+#python manage.py collectstatic --noinput
+#python manage.py makemigrations
+##
+#python manage.py migrate
+#
+## Start server
+#echo "Starting server"
+#
+#python manage.py flush --no-input
+#python manage.py collectstatic --no-input
+gunicorn --timeout=30 --workers=2 --bind 0.0.0.0:8001 cowrywise.wsgi:application --reload
+exec "$@"
