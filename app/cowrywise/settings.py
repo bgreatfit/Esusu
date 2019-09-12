ba7dbc50d2491cce5acf71d7eb7a176eb9bca319
micheal <mojemoron2009@yahoo.com>
2019-09-12 07:52:56 +0100

    Add nginx config

--- app/cowrywise/settings.py
+++ app/cowrywise/settings.py
@@ -167,8 +167,8 @@ SIMPLE_JWT = {
 
 }
 
-CELERY_BROKER_URL = os.environ['RDS_DB_NAME'] if 'RDS_DB_NAME' in os.environ else 'redis://redis:6379'
-CELERY_RESULT_BACKEND = os.environ['RDS_DB_NAME'] if 'RDS_DB_NAME' in os.environ else 'redis://redis:6379'
+CELERY_BROKER_URL = 'redis://:'+os.environ['REDIS_HOST'] if 'RDS_DB_NAME' in os.environ else 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://:'+os.environ['REDIS_HOST'] if 'RDS_DB_NAME' in os.environ else 'redis://redis:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
